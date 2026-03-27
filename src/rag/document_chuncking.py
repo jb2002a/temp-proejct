43b398e15333cee6710e59a61c30055b7ef0d847
jb2002a <2jaebin@gmail.com>
2026-03-27 15:25:22 +0900

    feat : Add document chunking and loading logic

---
+++ src/rag/document_chuncking.py
@@ -0,0 +1,21 @@
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import Document
+from llama_index.core.schema import BaseNode
+from typing import List
+from src.rag.document_loader import load_documents, pdf_file_path
+
+def chunk_document(document: List[Document]) -> List[BaseNode]:
+    splitter = SentenceSplitter()
+    all_chunks = []
+    for doc in document:
+        chunks = splitter.get_nodes_from_documents([doc], show_progress=True)
+        all_chunks.extend(chunks)
+    return all_chunks
+
+if __name__ == "__main__":  
+    documents = load_documents(pdf_file_path)
+    chunks = chunk_document(documents)
+    for chunk in chunks:
+        print(chunk.text)
+        print("-" * 100)
+
---
+++ src/rag/document_loader.py
@@ -0,0 +1,17 @@
+from llama_index.readers.file import PDFReader
+from llama_index.core import Document
+from typing import List
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+pdf_file_path = PROJECT_ROOT / "pdfs" / "당뇨병 임상진료지침.pdf"
+
+def load_documents(pdf_file_path: Path) -> List[Document]:
+    reader = PDFReader()
+    documents = reader.load_data(pdf_file_path)
+    return documents
+
+if __name__ == "__main__":
+    documents = load_documents(pdf_file_path)
+    print(documents)
+
