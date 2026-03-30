c6e40c1b455826488c332a6a51e9b76b94abf546
LeeJaeBin <2jaebin@gmail.com>
2026-03-31 02:40:11 +0900

    chore: Remove deprecated graph module files and nodes to streamline codebase

--- src/graph/state.py
+++
@@ -1,9 +0,0 @@
-from typing_extensions import TypedDict
-from typing import List
-from llama_index.core.schema import NodeWithScore
-
-class State(TypedDict):
-    query: str
-    context: List[str]
-    answer: str
-
