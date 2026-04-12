8ce2398f5a41bceb33c8413e2299959ee4069bfa
LeeJaeBin <2jaebin@gmail.com>
2026-04-12 17:50:10 +0900

    feat: Refactor test dataset generation to use document chunks, update model retrieval, and streamline output to CSV

--- src/rag/dataset/dataset_generate.py
+++ src/rag/dataset/dataset_generate.py
@@ -1,52 +1,27 @@
 
 from ragas.testset import TestsetGenerator
-from langsmith import Client
-
 from pandas import DataFrame
-from src.rag.common.clients import get_advanced_model, get_embed_model
-from src.rag.pre_processing.document_pre_proccessing import load_documents
+from src.rag.common.clients import get_model, get_embed_model
+from src.rag.pre_processing.document_pre_proccessing import load_documents, split_document
+from pathlib import Path
 
-import asyncio
-from ragas.testset.synthesizers.single_hop.specific import SingleHopSpecificQuerySynthesizer
-from ragas.testset.synthesizers.multi_hop import (
-    MultiHopSpecificQuerySynthesizer,
-    MultiHopAbstractQuerySynthesizer,
-)
-
-async def generate_test_dataset_and_store() -> DataFrame:
-    llm = get_advanced_model()
-
-    distribution = [
-        (SingleHopSpecificQuerySynthesizer(llm=llm), 0.5),
-        (MultiHopSpecificQuerySynthesizer(llm=llm), 0.25),
-        (MultiHopAbstractQuerySynthesizer(llm=llm), 0.25),
-    ]
-
-    for query, _ in distribution:
-        prompts = await query.adapt_prompts("korean", llm=llm)
-        query.set_prompts(**prompts)
+def generate_test_dataset_and_store() -> DataFrame:
+    llm = get_model()
 
     embedding_model = get_embed_model()
     documents = load_documents()
+    chunks = split_document(documents)
 
     generator = TestsetGenerator.from_langchain(llm=llm, embedding_model=embedding_model)
-    testset = generator.generate_with_langchain_docs(
-        documents=documents,
-        testset_size=1,
-        query_distribution=distribution,
-    )
-    df = testset.to_pandas()
-    return df
-
-def upload_pandas_with_langsmith(df: DataFrame) -> None:
-    client = Client()
-
-    input_keys = ["user_input", "reference_contexts"]
-    output_keys = ["reference", "response"]
-
-    client.upload_dataframe(df, name="ragas_testset", input_keys=input_keys, output_keys=output_keys)
+    testset = generator.generate_with_chunks(chunks=chunks, testset_size=20)
+    return testset.to_pandas()
 
 if __name__ == "__main__":
     # python -m src.rag.dataset.dataset_generate
-    generate_test_dataset_and_store()
+    df = generate_test_dataset_and_store()
+    output_path = Path("src/test_set/testset.csv")
+
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_path, index=False, encoding="utf-8-sig")
     print("Test dataset generated and stored")
+
